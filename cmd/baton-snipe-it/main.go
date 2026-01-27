969a8493052993236fd083b46f86c73635108fdb
Steve Gontzes <gontzess@gmail.com>
2026-01-27 17:12:37 -0500

    refactor: use generated config type instead of viper
    Update main.go to use SDK generated config type SnipeIt.

--- cmd/baton-snipe-it/main.go
+++ cmd/baton-snipe-it/main.go
@@ -7,13 +7,11 @@ import (
 
 	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
-	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
-	"github.com/spf13/viper"
 	"go.uber.org/zap"
 
-	snipeitconfig "github.com/conductorone/baton-snipe-it/pkg/config"
+	cfg "github.com/conductorone/baton-snipe-it/pkg/config"
 	"github.com/conductorone/baton-snipe-it/pkg/connector"
 )
 
@@ -26,8 +24,7 @@ func main() {
 		ctx,
 		"baton-snipe-it",
 		getConnector,
-		snipeitconfig.ConfigurationSchema,
-		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.SnipeIt{}),
+		cfg.Config,
 	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
@@ -43,24 +40,20 @@ func main() {
 	}
 }
 
-func getConnector(ctx context.Context, v *viper.Viper) (types.ConnectorServer, error) {
+func getConnector(ctx context.Context, c *cfg.SnipeIt) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
 
-	cb, err := connector.New(
-		ctx,
-		v.GetString(snipeitconfig.BaseUrlField.FieldName),
-		v.GetString(snipeitconfig.AccessTokenField.FieldName),
-	)
+	cb, err := connector.New(ctx, c.BaseUrl, c.AccessToken)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
 	}
 
-	c, err := connectorbuilder.NewConnector(ctx, cb)
+	conn, err := connectorbuilder.NewConnector(ctx, cb)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
 	}
 
-	return c, nil
+	return conn, nil
 }
