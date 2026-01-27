1d4ffd5cdeebb15d3074f62cfe2e67b4adae5ed8
Steve Gontzes <gontzess@gmail.com>
2026-01-27 15:20:46 -0500

    Upgrade baton-sdk to v0.7.4 and Go to 1.25.2

--- cmd/baton-snipe-it/main.go
+++ cmd/baton-snipe-it/main.go
@@ -5,12 +5,15 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/conductorone/baton-sdk/pkg/cli"
+	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
+	"github.com/spf13/viper"
 	"go.uber.org/zap"
 
+	snipeitconfig "github.com/conductorone/baton-snipe-it/pkg/config"
 	"github.com/conductorone/baton-snipe-it/pkg/connector"
 )
 
@@ -19,15 +22,19 @@ var version = "dev"
 func main() {
 	ctx := context.Background()
 
-	cfg := &config{}
-	cmd, err := cli.NewCmd(ctx, "baton-snipe-it", cfg, validateConfig, getConnector)
+	_, cmd, err := config.DefineConfiguration(
+		ctx,
+		"baton-snipe-it",
+		getConnector,
+		snipeitconfig.ConfigurationSchema,
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.SnipeIt{}),
+	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 
 	cmd.Version = version
-	cmdFlags(cmd)
 
 	err = cmd.Execute()
 	if err != nil {
@@ -36,10 +43,14 @@ func main() {
 	}
 }
 
-func getConnector(ctx context.Context, cfg *config) (types.ConnectorServer, error) {
+func getConnector(ctx context.Context, v *viper.Viper) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
 
-	cb, err := connector.New(ctx, cfg.BaseUrl, cfg.AccessToken)
+	cb, err := connector.New(
+		ctx,
+		v.GetString(snipeitconfig.BaseUrlField.FieldName),
+		v.GetString(snipeitconfig.AccessTokenField.FieldName),
+	)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
